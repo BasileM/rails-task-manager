fe51666a515bbe6ff117fed13b0680a7c6a39e58
Basile Marquefave <basile.marquefave@gmail.com>
2018-05-15 15:16:22 +0200

    affichage des tasks et checkbox

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,9 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :details, presence: true
 end
+
+# en ajoutant des validates, on oblige les champs a contenir une valeur
+# sinon ce n'est pas ajouté dans la BDD
+# je pourrais aussi seulement demander title, ce qui me permet quand meme de l'ajouter
+# a la base de donné et de pouvoir modifier plus tard les détails.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Create
+  # Read
+  # Update
+  # Delete
+  get "tasks", to: "tasks#index"
 end
