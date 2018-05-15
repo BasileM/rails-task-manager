7b0587b171a5ae3d0ba634682b5e3909bc1b3f91
Basile Marquefave <basile.marquefave@gmail.com>
2018-05-15 17:34:03 +0200

    VICTORY

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,22 @@ Rails.application.routes.draw do
   # Read
   # Update
   # Delete
+
+  #lire toutes les tasks
   get "tasks", to: "tasks#index"
+
+  #create a new
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  #lire une tasks
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  #edit
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update", as: :update
+
+  # delete
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
+
 end
